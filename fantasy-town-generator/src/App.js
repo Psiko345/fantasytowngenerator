f120cdd5416cbe3aaa2535d9855af86fa7716a0f
Joshua Piper <joshua.g.piper@gmail.com>
2021-04-05 15:39:35 +1000

    Added a light/dark mode to refresh my memory of react context.

--- fantasy-town-generator/src/App.js
+++ fantasy-town-generator/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import Header from './Header'
+import Main from './MainWithClass'
+import ThemeContext from './Context/ThemeContext';
 
 function App() {
+  const themeHook = useState("light");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <Header />
+        <Main />
+
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
